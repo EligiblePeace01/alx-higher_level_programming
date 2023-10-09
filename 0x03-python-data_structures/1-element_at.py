c2e4d356654c611ef59fea63cd6e9edf45b579cf
EligiblePeace01 <enwerempeace@gmail.com>
2023-10-09 01:00:15 -0700

    Python Data Structures using lists and tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 1-element_at.py
-
-
 def element_at(my_list, idx):
-    """Retrive an element from a list."""
-    if idx < 0 or idx > (len(my_list) - 1):
+    if idx < 0 or idx >= len(my_list):
         return None
-    return (my_list[idx])
-
+    else:
+        return my_list[idx]
