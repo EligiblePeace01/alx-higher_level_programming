bc93d1b9dcc58a89990c421f31113fa9c96b89be
EligiblePeace01 <enwerempeace@gmail.com>
2023-11-06 11:11:10 -0800

    create new classes that inherit attributes and behaviors from existing classes

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+
+"""
+module for BaseGeometry.
+"""
 
 
-class BaseGeometry:
-    """Reprsent base geometry."""
+class BaseGeometry():
+    """BaseGeometry class"""
 
     def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented yet")
+        """Raise an Exception"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
+        """Validate"""
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(name + " must be greater than 0")
