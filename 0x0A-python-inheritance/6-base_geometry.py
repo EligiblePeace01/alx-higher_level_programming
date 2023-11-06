6ef981858977575101e24c45bb1180bfe6e70aa3
EligiblePeace01 <enwerempeace@gmail.com>
2023-11-06 10:57:31 -0800

    create new classes that inherit attributes and behaviors from existing classes

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module returns the list of available attributes
+    and methods of an object
+""""""
+    This module returns the list of available attributes
+    and methods of an object
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
