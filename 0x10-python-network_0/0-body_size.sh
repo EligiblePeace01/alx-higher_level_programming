e3365924422af1123e1389bc555cbdc88f301a63
EligiblePeace01 <enwerempeace@gmail.com>
2024-01-29 02:35:00 -0800

    fix my code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# body size
-curl -sI $1 | grep "Content-Length:" | cut -d' ' -f2
+# Sends a request to that URL, and displays the size of the body of the response
+curl -s "${1}" | wc -c
